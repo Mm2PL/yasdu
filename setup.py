684935df66bc3ab40ea705e39c91a831ea5a5ca2
Mm2PL <jakis128@gmail.com>
2020-09-21 15:58:01 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='yasdu',
+    version='1.0.0',
+    author='Mm2PL',
+    author_email='mm2pl+yasdu@o2.pl',
+    description='Yet another stack/session dumper utility',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mm2pl/yasdu',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'yasdu=yasdu.__main__:_main'
+        ]
+    }
+)
